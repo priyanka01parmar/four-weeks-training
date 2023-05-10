0dc711a6e857618ecf33691c88c19afcabfb672f
priyanka parmar <priyanka01paramr1601@gmail.com>
2023-05-10 14:16:39 +0530

    Circle exercise 1

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Circle
+﻿// Execercise no.1 OOP concepts
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Circle
 {
-    internal class Program
+    class Circle
     {
+        public double Rad { get; private set; }
+
         static void Main(string[] args)
         {
-            // Create a Circle object and display its area and circumference
+            double rad = 0;
+            Console.Write("Enter a Radius:");
+            rad = Convert.ToDouble(Console.ReadLine());
+            Circle c = new Circle(rad);
+            Console.WriteLine("Area OF circle :" + c.GetArea());
+            Console.WriteLine("Area of circumference:" + c.Getcircumference());
+        }
+
+
+
+        public Circle(double rad)
+        {
+
+            this.Rad = rad;
+        }
+        public double GetArea()
+        {
+
+            double pi = 0;
+            double area = 0;
+            pi = 3.14;
+            area = pi * this.Rad * this.Rad;
+            return area;
+        }
+        public double Getcircumference()
+        {
+            double circum = 0;
+            double pi = 0;
+            pi = 3.14;
+            circum = 2 * pi * this.Rad;
+            return circum;
         }
     }
 }
