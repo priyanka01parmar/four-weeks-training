ecafe6951ab95f0cb3492d55d1c2fbb48b8e9985
priyanka parmar <priyanka01parmar1601@gmail.com>
2023-05-11 17:55:57 +0530

    LinQtoObject

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -1,20 +1,31 @@
-﻿namespace IteratorsApp
+﻿using System;
+using System.ComponentModel;
+namespace IteratorsApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var fibonacci = FibonacciSequence().Take(10);
+            var fibonacci = FibonacciSequence(8);
             foreach (int number in fibonacci)
             {
                 Console.WriteLine(number);
             }
         }
-        // https://www.c-sharpcorner.com/UploadFile/5ef30d/understanding-yield-return-in-C-Sharp/
-        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
-        public static IEnumerable<int> FibonacciSequence()
+        
+        public static IEnumerable<int> FibonacciSequence(int length)
         {
-            throw new NotImplementedException();
+            int x = 0, y = 1;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sum += x;
+                yield return x;
+                int c = x + y;
+                x = y;
+                y = c;
+            }
+            Console.WriteLine($"Sum of Fibonacci:{sum}");
         }
     }
 }
