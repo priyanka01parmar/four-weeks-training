464042710c5cae36ea87e5c5afc41f51c4e4706d
priyanka parmar <priyanka01parmar1601@gmail.com>
2023-05-11 17:57:02 +0530

    Fibonacci with iterator

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -1,31 +1,32 @@
-﻿namespace LinqToObjectsApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Person
 {
-    internal class Program
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Country { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<Person> people = new List<Person>()
         {
-            List<Person> people = new List<Person>
-            {
-                new Person { Name = "John", Age = 25, Country = "USA" },
-                new Person { Name = "Jane", Age = 30, Country = "Canada" },
-                new Person { Name = "Mark", Age = 28, Country = "USA" },
-                new Person { Name = "Emily", Age = 22, Country = "Australia" }
-            };
-
-
-            //Write queries using LINQ for following operations
-            //1. Get all people from USA
-            //2. Get all people above 30
-            //3. Sort people by name
-            //4. Project/Select only Name and Country of all people
-
+            new Person { Name = "John", Age = 25, Country = "USA" },
+            new Person { Name = "Emma", Age = 30, Country = "Canada" },
+            new Person { Name = "Michael", Age = 35, Country = "USA" },
+            new Person { Name = "Sophia", Age = 28, Country = "Canada" },
+            new Person { Name = "Daniel", Age = 32, Country = "USA" }
+        };
+        int filterAge = 30;
+        var filteredPeople = people.Where(p => p.Age > filterAge);
+        var sortedPeople = filteredPeople.OrderBy(p => p.Name);
+        var projectedPeople = sortedPeople.Select(p => new { p.Name, p.Country });
+        foreach (var person in projectedPeople)
+        {
+            Console.WriteLine($"Name: {person.Name}, Country: {person.Country}");
         }
     }
-
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Country { get; set; }
-    }
 }
+
