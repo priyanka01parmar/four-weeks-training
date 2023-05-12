e76fc5bfaad9fdfad9ae190f473b47a2e23cd588
priyanka parmar <priyanka01parmar1601@gmail.com>
2023-05-12 16:43:19 +0530

    Covariance_Contravariance

--- week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
+++ week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
@@ -1,32 +1,46 @@
-﻿namespace CovariantAndContravariantGenerics
+﻿using System.ComponentModel;
+namespace CovariantAndContravariantGenerics
 {
     interface IProcessor<in TInput, out TResult>
     {
         TResult Process(TInput input);
     }
 
-    class StringToIntProcessor : IProcessor<string, int>
+    public class StringToIntProcessor : IProcessor<string, int>
     {
-        // Implement Process method
         public int Process(string input)
         {
-            throw new NotImplementedException();
+            return input.Length;
         }
     }
 
-    class DoubleToStringProcessor : IProcessor<double, string>
+    public class DoubleToStringProcessor : IProcessor<double, string>
     {
-        // Implement Process method
         public string Process(double input)
         {
-            throw new NotImplementedException();
+            return input.ToString();
         }
     }
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            // Demonstrate covariance and contravariance with IProcessor interface
+            // Covariance example: StringToIntProcessor can be used as an IProcessor<object, int>
+            IProcessor<string, int> intProcessor = new StringToIntProcessor();
+            int result = intProcessor.Process("hello");
+            Console.WriteLine($"String length: {result}");
+
+            // Contravariance example: IProcessor<string, int> can be used as an IProcessor<object, int>
+
+            IProcessor<string, int> contravariantProcessor = new StringToIntProcessor();
+            int length = contravariantProcessor.Process("hello");
+            Console.WriteLine($"String length: {length}");
+
+            // Covariance example: DoubleToStringProcessor can be used as an IProcessor<double, object>
+            IProcessor<double, string> stringProcessor = new DoubleToStringProcessor();
+            object str = stringProcessor.Process(3.14);
+            Console.WriteLine($"String representation of 3.14: {str}");
         }
     }
 }
