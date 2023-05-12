7bc5679710b8842306b5654108f86516ccb5f6c7
priyanka parmar <priyanka01parmar1601@gmail.com>
2023-05-12 16:45:27 +0530

    FileLoggerDisposal

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -1,2 +1,45 @@
-﻿
+﻿namespace FileLoggerDisposableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var logger = new FileLogger("log.txt"))
+            {
+                logger.Log("This is a log message.");
+                logger.Log("This is another log message.");
+            }
+        }
+    }
+    class FileLogger : IDisposable
+    {
+        private StreamWriter _writer;
+        public FileLogger(string filePath)
+        {
+            _writer = new StreamWriter(filePath);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_writer != null)
+                {
+                    _writer.Dispose();
+                    _writer = null;
+                }
+            }
+        }
+        public void Log(string message)
+        {
+            {
+                _writer.WriteLine($"{DateTime.UtcNow}: {message}");
+                _writer.Flush();
+            }
+        }
+    }
 }
