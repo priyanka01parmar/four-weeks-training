f6db9c2101ed0dab166abe94dcccec678d1c8d1a
priyanka parmar <priyanka01parmar1601@gmail.com>
2023-05-12 16:57:36 +0530

    LinQList_number

--- week-1/day-4/exercise-7/LinqListNumbers/Program.cs
+++ week-1/day-4/exercise-7/LinqListNumbers/Program.cs
@@ -1,18 +1,36 @@
-﻿namespace LinqListNumbers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> numbers = new List<int> { 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+        // Find all even numbers
+        var evenNumbers = numbers.Where(n => n % 2 == 0);
+        Console.WriteLine("Even numbers:");
+        foreach (var number in evenNumbers)
         {
-            // Create a list of integers
-            List<int> numbers = new List<int> { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
-
-            // Use LINQ to perform the following operations:
-            // 1. Find all even numbers
-            // 2. Find all numbers greater than a specific value (e.g., 20)
-            // 3. Calculate the sum of all numbers
-            // 4. Calculate the average of all numbers
-            // 5. Find the minimum and maximum values in the list
+            Console.WriteLine(number);
         }
+        // Find all numbers greater than 20
+        int threshold = 20;
+        var greaterThanThreshold = numbers.Where(n => n > threshold);
+        Console.WriteLine("\nNumbers greater than 20:");
+        foreach (var number in greaterThanThreshold)
+        {
+            Console.WriteLine(number);
+        }
+        // Calculate the sum of all numbers
+        int sum = numbers.Sum();
+        Console.WriteLine($"\nSum of all numbers: {sum}");
+        // Calculate the average of all numbers
+        double average = numbers.Average();
+        Console.WriteLine($"Average of all numbers: {average}");
+        // Find the minimum and maximum values
+        int min = numbers.Min();
+        int max = numbers.Max();
+        Console.WriteLine($"\nMinimum value: {min}");
+        Console.WriteLine($"Maximum value: {max}");
     }
 }
